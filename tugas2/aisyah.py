32cb61633124112292f1a0ae97a5de1f4aef0b97
nuraisyah4 <nur.22315043@umkendari.ac.id>
2023-11-05 07:34:36 -0800

    Tugas 2 selesai

---
+++ tugas2/aisyah.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__" :
+	# tuple
+	x = ( 1 , "Belimbing" ,"3.14" ,"ceri" )
+	# list
+	y = [ 1 , "Belimbing" ,"3.14" ,"ceri" ]
+	#dictionary
+	z = {
+		1 : "Apel" , "b" : "Belimbing" ,  "c" : "ceri"	, "x" : "xemangka"
+			}
+	
+print(type(x))
+print(x)
+print(type(y))
+print(y)
+print(type(z))
+print(z)
